378633af07ecdda327b4cda2f1094b6a5a64ffb2
Kerim Şentürk <kerimsenturk2002@outlook.com>
2023-08-04 16:05:20 +0300

    Project Initialize. Firs commit

---
+++ src/main/java/com/kerimsenturk/linkbranch/LinkbranchApplication.java
@@ -0,0 +1,13 @@
+package com.kerimsenturk.linkbranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkbranchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkbranchApplication.class, args);
+    }
+
+}
